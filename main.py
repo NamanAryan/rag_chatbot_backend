92c77a47952c04eaaa6c3f1db024f4c30111d05a
NamanAryan <namanaryan08@gmail.com>
2025-06-30 13:16:50 +0530

    refactor: Simplify CORS origins configuration by using a variable for allow_origins

--- main.py
+++ main.py
@@ -29,14 +29,15 @@ from pathlib import Path
 app = FastAPI()
 llm = GeminiLLM()
 load_dotenv()
-
+origins = [
+    "http://localhost:3000", 
+    "http://127.0.0.1:3000",
+    "http://localhost:5173",  
+    "http://127.0.0.1:5173",
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",  
-        "http://localhost:3000", 
-        "http://127.0.0.1:5173",  
-    ],  
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
