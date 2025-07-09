910f31b65dcda875bb71f9a1265f2cdb283b01c3
NamanAryan <namanaryan08@gmail.com>
2025-07-09 12:37:47 +0530

    feat: Add frontend URL to CORS origins for enhanced access control

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ origins = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",  
     "http://127.0.0.1:5173",
+    "https://neuraai-frontend.vercel.app"
 ]
 app.add_middleware(
     CORSMiddleware,
